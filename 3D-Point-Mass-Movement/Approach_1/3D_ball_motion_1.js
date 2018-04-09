3bbea3df9c57d0facc7e6cfb8f6519a5068d8edb
aibearde <aibearde@iupui.edu>
2018-04-08 20:49:04 -0400

    Small changes

---
+++ 3D-Point-Mass-Movement/Approach_1/3D_ball_motion_1.js
@@ -0,0 +1,87 @@
+//Author: Austin Bearden
+//Date Created: 3/16/2108
+//Project: PhysicsFoosball Approach 1
+
+//Special thank to schteppe for providing cannon.js physics engine on github for public use.
+//Here's a link: https://github.com/schteppe/cannon.js 
+
+//execute calculations when button is clicked
+function calculateAll() {
+
+    //get values from input boxes
+    //my problem is here right here and Now I am going to solve it!!!!
+    var X_input = parseFloat(document.getElementById('Xcoord').value);
+    console.log(X_input);
+    var Y_input = parseFloat(document.getElementById('Ycoord').value);
+    console.log(Y_input);
+    var Z_input = parseFloat(document.getElementById('Zcoord').value);
+    console.log(Z_input);
+    var speed = parseFloat(document.getElementById('SpeedInp').value);
+    console.log(speed);
+    var XYangle = parseFloat(document.getElementById('XYDirection').value);
+    console.log(XYangle);
+    var XZangle = parseFloat(document.getElementById('XZDirection').value);
+    console.log(XZangle);
+    var time = parseFloat(document.getElementById('TimeInp').value);
+    console.log(time);
+
+    //create the world
+    var ball_world = new CANNON.World();
+    ball_world.gravity.set(0,0,-9.81); // meters per seconds-squared
+
+    //velocity components
+    var V_xy = speed*Math.cos(XZangle*Math.PI/180);
+    var V_z = speed*Math.sin(XZangle*Math.PI/180);
+    var V_y = V_xy*Math.sin(XYangle*Math.PI/180);
+    var V_x = V_xy*Math.cos(XYangle*Math.PI/180);
+
+    //create the ball
+    var radius = 6; // meters
+    var ball_body = new CANNON.Body({
+        mass: 0.005,
+        position: new CANNON.Vec3(X_input,Y_input,Z_input), // meters
+        velocity: new CANNON.Vec3(V_x, V_y, V_z) // speed in meters per second
+
+    });
+    ball_body.linearDamping = 0.0;
+
+    ball_world.addBody(ball_body);
+
+    // Create floor of ball world
+    var world_ground = new CANNON.Body({
+
+        mass: 0 // mass == 0 makes the body static
+
+    });
+
+    var ground_shape = new CANNON.Plane();
+    world_ground.addShape(ground_shape);
+    ball_world.addBody(world_ground);
+
+    var ball_contact = new CANNON.ContactMaterial( ball_world, ball_body, 0.0, 0.0, 0.0);
+
+    ball_world.addContactMaterial(ball_contact);
+
+    //var fixed_time_step = 1.0 / 1.0; // seconds (So, steps per seconds or second per steps ??)
+    //var max_sub_steps = 1; // figure out these last two lines
+
+        ball_world.step(time);
+
+        var X_final = ball_body.position.x;
+        var Y_final = ball_body.position.y;
+        var Z_final = ball_body.position.z;
+        //output to HTML page
+        document.getElementById("x-axis").innerHTML = "X-Position: " + X_final + " meters";
+        console.log("X position: " + ball_body.position.x);
+        //output to HTML page
+        document.getElementById("y-axis").innerHTML = "Y-Position: " + Y_final + " meters";
+        console.log("Y position: " + ball_body.position.y);
+        //output to HTML page
+        document.getElementById("z-axis").innerHTML = "Z-Position: " + Z_final + " meters";
+        console.log("Z position: " + ball_body.position.z);
+
+}
+
+//Accesses button from html	
+var calculate = document.getElementById("calculate");
+calculate.addEventListener("click", function() { calculateAll(); } );
---
+++ 3D-Point-Mass-Movement/README.md
@@ -0,0 +1,11 @@
+# PhysicsFoosball
+Will keep track of my progress of particular components of my physics engine for my virtual foosball game.
+
+Project 1:
+
+  Model the movement of a ball in 3D space. Tackle the problem twice. Each time with a different approach
+   
+     Approach 1: Write the program in .js and use a fullfledged physics engine to model the movements
+     
+     Approach 2: Write the program in .js but this time don't use any physics libraries,
+     write the whole program yourself
